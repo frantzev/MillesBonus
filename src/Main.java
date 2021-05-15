d0ccc10c0061cfd3ed7bb8bbe1a2d8ea6f8bb3f0
Aleksey Frantsev <frantzev@yandex.ru>
2021-05-16 00:12:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 11540;
+        int bonus = price / 20;
+
+        System.out.println("Вам начислено " + bonus + " миль");
+    }
+}
